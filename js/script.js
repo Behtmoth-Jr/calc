0964e1ad8a6650c628f5682919fd0d06a2e7c14b
Behtmoth-Jr <79050532+Behtmoth-Jr@users.noreply.github.com>
2023-10-11 01:58:51 +0300

    2

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const result = {
+    data () {
+        return {
+            result:'',
+            numbers: [1,2,3,4,5,6,7,8,9,",",0,"00"],
+            operators: ["+","-","/","*","(",")"]
+        }
+    },
+    methods: {
+        input (char) {
+            this.result+=char;
+        },
+        calculate () {
+            this.result=eval(this.result);
+        },
+        reset () {
+            this.result="";
+        }
+    }
+}
+Vue.createApp(result).mount('#calc')
